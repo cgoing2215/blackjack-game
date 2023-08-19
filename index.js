d7e99c6ca786e3754bbd8211b0a998d25582b333
Caroline Going <carolinegoing2@gmail.com>
2023-08-19 09:54:02 -0400

    Updated background & CSS values, added an updateChipValue function and classList changes upon winning or losing

--- index.js
+++ index.js
@@ -9,22 +9,37 @@ let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 let messageEl = document.querySelector("#message-el");
 let playerEl = document.querySelector("#player-el")
+let button = document.querySelectorAll(".button")
+const input = document.querySelector("input");
+
 let player = {
-    name: "Caroline",
+    name: "",
     chips: 500,
 }
 
-playerEl.textContent = player.name + ": $" + player.chips
+// allow interactive name adding
+
+input.addEventListener("input", updateValue);
+
+function updateValue(e) {
+  player.name = e.target.value;
+
+  playerEl.textContent = player.name + ": $" + player.chips;
+}
 
 // starting the game
 
 function startGame(){
+    messageEl.classList.remove("lose");
+    messageEl.classList.remove("win");
+
     isStillInGame = true;
     let firstCard = randomCard();
     let secondCard = randomCard();
     sum = firstCard + secondCard;
     cards = [firstCard, secondCard]
     renderGame();
+    updateChipValue();
 }
 
 function renderGame(){
@@ -39,11 +54,33 @@ function renderGame(){
     } else if (sum === 21){
         message = "You've got blackjack!"
         hasBlackjack = true;
+        messageEl.classList.add("win");
     } else {
         message = "You're out of the game"
         isStillInGame = false;
+        messageEl.classList.add("lose");
     }
     messageEl.textContent = message
+    updateChipValue();
+}
+
+// updating chip value
+function updateChipValue(){
+    if (sum > 21) {
+        player.chips -= 50;
+    } else if (sum === 21){
+        player.chips += 50;
+    } else {
+        return player.chips;
+    }
+
+    playerEl.textContent = player.name + ": $" + player.chips;
+
+    if (player.chips < 0){
+        cardsEl.classList.add("hidden");
+        sumEl.classList.add("hidden");
+        button.classList.add("hidden")
+    }
 }
 
 // drawing cards
